6736767ade0bbdee0834090fd8aa852a1c97b09f
BigBird-HQ <yaromeshack@gmail.com>
2024-02-06 05:49:27 +0100

    first commit/new python tasks completed

---
+++ main.py
@@ -0,0 +1,32 @@
+def my_function():
+    print('Hello world')
+
+my_function()
+
+
+def subtraction():
+    print(3 - 2)
+
+subtraction()
+
+
+def add_nums(num1, num2):
+    """with args"""
+    print(3 + 2)
+
+add_nums(3, 2)
+
+def add_nums():
+    print(3 + 2)
+
+add_nums()
+
+
+
+def multiplyNum(num):
+    return num * 4
+
+result = multiplyNum(2)
+print(result)
+
+

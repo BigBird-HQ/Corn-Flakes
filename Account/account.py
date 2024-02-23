cbf67ef2d34ed22f6e397518b141432643f1c186
MeshackYaro <yaromeshack@gmail.com>
2024-02-23 22:44:27 +0100

    updating newly completed tasks

--- Account/account.py
+++ Account/account.py
@@ -1,4 +1,6 @@
+from Account.InsufficientFundsError import InsufficientFundsError
 from Account.InvalidAmountError import InvalidAmountError
+from Account.InvalidPinError import InvalidPinError
 
 
 class Account:
@@ -17,8 +19,20 @@ class Account:
             raise InvalidAmountError('Invalid Amount')
         self.balance += amount
 
-    def withdraw(self, amount):
+    def withdraw(self, amount, pin):
+        self.insufficient_funds(amount)
+        self.invalid_amount(amount)
+        self.validate(pin)
         self.balance -= amount
 
+    def validate(self, pin):
+        if not pin == self.pin:
+            raise InvalidPinError('Invalid Pin')
 
+    def invalid_amount(self, amount):
+        if amount == 0:
+            raise InvalidAmountError('Invalid Amount')
 
+    def insufficient_funds(self, amount):
+        if amount > self.balance:
+            raise InsufficientFundsError('Insufficient Funds')
--- Account/accountTest.py
+++ Account/accountTest.py
@@ -1,22 +1,23 @@
 import unittest
 
+from Account.InsufficientFundsError import InsufficientFundsError
 from Account.InvalidAmountError import InvalidAmountError
+from Account.InvalidPinError import InvalidPinError
 from Account.account import Account
 
 
 class MyTestCase(unittest.TestCase):
     def test_that_account_is_empty_at_creation(self):
-
         account = Account('mavericks', 0, '1234', 1)
 
         self.assertEqual(0, account.check_balance('1234'))
 
-    def est_that_balance_increases_when_positive_amount_is_deposited(self):
+    def test_that_balance_remains_unchanged_when_zero_amount_is_deposited(self):
         account = Account('mavericks', 0, '1234', 1)
         account.deposit(0)
         self.assertEqual(0, account.check_balance('1234'))
 
-    def test_that_positive_amount_amount_can_be_deposited(self):
+    def test_that_balance_increases_when_positive_amount_is_deposited(self):
         account = Account('mavericks', 0, '1234', 1)
         account.deposit(2000)
         self.assertEqual(2000, account.check_balance('1234'))
@@ -37,21 +38,30 @@ class MyTestCase(unittest.TestCase):
     def test_that_balance_decreases_when_positive_amount_is_withdrawn(self):
         account = Account('mavericks', 0, '1234', 1)
         account.deposit(5000)
-        account.withdraw(3000)
+        account.withdraw(3000, '1234')
         self.assertEqual(2000, account.check_balance('1234'))
 
-    def test_that_balance_remains_unchanged_when_zero_amount_is_withdrawn(self):
+    def test_that_when_zero_amount_is_withdrawn_it_throws_invalid_amount_error(self):
         account = Account('mavericks', 0, '1234', 1)
-        account.withdraw(0)
+        with self.assertRaises(InvalidAmountError):
+            account.withdraw(0, '1234')
         self.assertEqual(0, account.check_balance('1234'))
 
+    def test_that_withdrawal_amount_cannot_be_greater_than_balance_throw_insufficient_funds_exception(self):
+        account = Account('mavericks', 0, '1234', 1)
+        account.deposit(2000)
+        self.assertEqual(2000, account.check_balance('1234'))
+        with self.assertRaises(InsufficientFundsError):
+            account.withdraw(3000, '1234')
+        self.assertEqual(2000, account.check_balance('1234'))
 
-
-
-
-
-
-
+    def test_that_wrong_pin_does_not_allow_Withdrawal_throws_invalid_pin_error(self):
+        account = Account('mavericks', 0, '1234', 1)
+        account.deposit(2000)
+        self.assertEqual(2000, account.check_balance('1234'))
+        with self.assertRaises(InvalidPinError):
+            account.withdraw(2000, '1233')
+        self.assertEqual(2000, account.check_balance('1233'))
 
 
 if __name__ == '__main__':

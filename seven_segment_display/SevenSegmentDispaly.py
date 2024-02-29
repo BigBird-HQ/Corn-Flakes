37935ab127d69661c62e675a1ef7821bc4fc39c2
MeshackYaro <yaromeshack@gmail.com>
2024-02-29 08:19:10 +0100

    updating newly completed tasks

--- seven_segment_display/SevenSegmentDispaly.py
+++ seven_segment_display/SevenSegmentDispaly.py
@@ -1,18 +1,23 @@
 def seven_segment_display(value):
     my_display = []
     my_display += value
+    horizontal_display(my_display[0])
+    vertical_display(my_display[5], my_display[1])
+    horizontal_display(my_display[6])
+    vertical_display(my_display[4], my_display[2])
+    horizontal_display(my_display[3])
 
 
 def horizontal_display(num):
     if num == '1':
-        print('*******')
+        print('**********')
 
 
 def vertical_display(num1, num2):
     if num1 == '1' and num1 == '1':
         for num in range(4):
             print('*        *')
-    elif num1 == '0' and num2:
+    elif num1 == '0' and num2 == '1':
         for num in range(4):
             print('           *')
     elif num1 == '1' and num2 == '0':
@@ -20,5 +25,5 @@ def vertical_display(num1, num2):
             print('*           ')
 
 
-num1 = ''
+num1 = '1111111'
 seven_segment_display(num1)
